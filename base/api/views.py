311c1191bb66c8bd4061c73e825cd05dc5f33d0c
HaoNhat <haonhatht@gmail.com>
2024-12-20 15:51:11 +0700

    add new sample database

--- base/api/views.py
+++ base/api/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from .ImageDatabase import ImageDatabase
 from PIL import Image
 
-from .const import results
+# from .const import results
 from .utils import find_image_from_cloudinary
 
 
@@ -19,25 +19,25 @@ def query(request):
     if request.method == "POST":
         query_image = request.FILES["query_image"]
 
-        # image = Image.open(query_image).convert("RGB")
+        image = Image.open(query_image).convert("RGB")
 
-        # my_db = ImageDatabase(model_path="models/model.pt")
-        # results = my_db.search(image)
+        my_db = ImageDatabase(db_path="sample_vectors.db", model_path="models/model.pt")
+        results = my_db.search(image)
         res = []
         # real version
-        # for result in results[0]:
-        #     a = result["entity"]
-        #     a.pop("vector")
-        #     res.append(a)
-        #     image_url = find_image_from_cloudinary(a["image_filename"])
-        #     res[-1]["image_url"] = image_url
+        for result in results[0]:
+            a = result["entity"]
+            a.pop("vector")
+            res.append(a)
+            image_url = find_image_from_cloudinary(a["image_filename"])
+            res[-1]["image_url"] = image_url
         #     print(f"Server: {a}")
         
         # test version:
-        for result in results:
-            image_url = find_image_from_cloudinary(result["image_filename"])
-            res.append(result)
-            res[-1]["image_url"] = image_url
+        # for result in results:
+        #     image_url = find_image_from_cloudinary(result["image_filename"])
+        #     res.append(result)
+        #     res[-1]["image_url"] = image_url
             # print(res[-1])
 
         return JsonResponse(
